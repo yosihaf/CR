278d68ae7680546c46f2e2d7c1945f7489f6b8ea
yosef <yoseff2525@gmail.com>
2018-04-29 17:45:39 +0300

    finished

--- app/src/main/java/com/example/user/carrentalapplication/controller/AddCarActivity.java
+++ app/src/main/java/com/example/user/carrentalapplication/controller/AddCarActivity.java
@@ -13,12 +13,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.user.carrentalapplication.R;
 import com.example.user.carrentalapplication.model.backend.DBManagerFactory;
 import com.example.user.carrentalapplication.model.backend.DB_manager;
 import com.example.user.carrentalapplication.model.datasource.List_DBManager;
+import com.example.user.carrentalapplication.model.entities.Branch;
 import com.example.user.carrentalapplication.model.entities.Car;
 import com.example.user.carrentalapplication.model.entities.CarModel;
 import com.example.user.carrentalapplication.model.entities.Gearbox;
@@ -28,17 +30,22 @@ import java.util.List;
 
 public class AddCarActivity extends BaseActivity implements View.OnClickListener {
 
-    private EditText brunchNum,kilometers,carNum;
+    private EditText kilometers,carNum ;
+    private TextView  model,branch;
     private Button addCar;
-    private Spinner modelNum;
-
-
+    private Spinner modelNum,brunchNum;
+    int indexModel=0;
+    int indexBranch=0;
 
     private void findViews() {
+        model= (TextView) findViewById(R.id.modelView);
+        branch= (TextView) findViewById(R.id.BranchView);
         addCar = (Button) findViewById(R.id.OK_addCar_btn);
-        brunchNum=(EditText) findViewById(R.id.editBrunch);
+        brunchNum=(Spinner) findViewById(R.id.spinnerBrunch);
+        brunchNum.setAdapter(new ArrayAdapter<Long>(this, android.R.layout.simple_spinner_item, brunchNum()));
         modelNum=(Spinner) findViewById(R.id.spinerModelNum);
         modelNum.setAdapter(new ArrayAdapter<Long>(this, android.R.layout.simple_spinner_item, codeModel()));
+
         kilometers=(EditText) findViewById(R.id.editKilometers);
         carNum=(EditText) findViewById(R.id.editCarNum);
 
@@ -48,13 +55,25 @@ public class AddCarActivity extends BaseActivity implements View.OnClickListener
 
     }
 
-    List<Long> codeModel()
+    private List<Long> brunchNum() {
+        List<Long> lst = new ArrayList<Long>() ;
+        String s;
+        for (Branch item:DBManagerFactory.getManager().getAllBrunches())
+        {
+          s= item.getAdress();
+            lst.add((long) item.getBranchNumber());
+        }
+        return lst;
+    }
+
+    private List<Long> codeModel()
     {
         List<Long> lst = new ArrayList<Long>() ;
-
+        String s;
 
         for (CarModel item:DBManagerFactory.getManager().getAllModels())
         {
+            s=item.getModel();
             lst.add(item.getCode());
         }
         return lst;
@@ -71,11 +90,6 @@ public class AddCarActivity extends BaseActivity implements View.OnClickListener
     @Override
     public void onClick(View v) {
         if(v==addCar)
-            if (TextUtils.isEmpty(brunchNum.getText().toString())) {
-                brunchNum.setError("Field must be full");
-                brunchNum.setHintTextColor(Color.RED);}
-
-            else
             if (TextUtils.isEmpty(kilometers.getText().toString())) {
                 kilometers.setError("Field must be full");
                 kilometers.setHintTextColor(Color.RED);}
@@ -84,6 +98,8 @@ public class AddCarActivity extends BaseActivity implements View.OnClickListener
                 carNum.setError("Field must be full");
                 carNum.setHintTextColor(Color.RED);}
             else {
+
+
                 AddCar();
                 Intent intent = new Intent(this, CarActivity.class);
                 startActivity(intent);
@@ -97,9 +113,13 @@ public class AddCarActivity extends BaseActivity implements View.OnClickListener
 
     private Car NewCar()
     {
-        Integer brunch = Integer.parseInt(brunchNum.getText().toString());
-        Integer model = Integer.parseInt(modelNum.getSelectedItem().toString());
+        Integer brunch = 0;
+        Integer model=0;
 
+        brunch= Integer.parseInt(brunchNum.getSelectedItem().toString());
+
+
+        model = Integer.parseInt(modelNum.getSelectedItem().toString());
         Integer km = Integer.parseInt(kilometers.getText().toString());
         Integer car = Integer.parseInt(carNum.getText().toString());
         return new Car(brunch,model,km,car);
