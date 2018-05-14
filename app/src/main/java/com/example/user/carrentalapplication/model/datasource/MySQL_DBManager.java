20441d8b026d5260234726a27144db9f619d5ecd
chaimke77 <chaimke77@gmail.com>
2018-05-14 23:59:54 +0300

    chaim

--- app/src/main/java/com/example/user/carrentalapplication/model/datasource/MySQL_DBManager.java
+++ app/src/main/java/com/example/user/carrentalapplication/model/datasource/MySQL_DBManager.java
@@ -9,6 +9,7 @@ import com.example.user.carrentalapplication.model.entities.Branch;
 import com.example.user.carrentalapplication.model.entities.Car;
 import com.example.user.carrentalapplication.model.entities.CarModel;
 import com.example.user.carrentalapplication.model.entities.Customer;
+import com.example.user.carrentalapplication.model.entities.Gearbox;
 import com.example.user.carrentalapplication.model.entities.User;
 
 import org.json.JSONArray;
@@ -26,7 +27,17 @@ import java.util.List;
 public class MySQL_DBManager implements DB_manager {
 
     private String WEB_URL ="http://crottenb.vlab.jct.ac.il/CR/";
+    private List<Customer> customerList;
+    private List<Branch> branchList;
+    private List<CarModel> carModelList;
+    private List<Car> carList;
 
+    public MySQL_DBManager(){
+        customerList = getAllCustomers();
+        branchList = getAllBrunches();
+        carModelList = getAllModels();
+        carList = getAllCars();
+    }
     @Override
     public boolean AddUser(User user) {
         return false;
@@ -44,31 +55,44 @@ public class MySQL_DBManager implements DB_manager {
 
     @Override
     public boolean ReturnCarById(Long values) {
+
+        for (Car item:carList) {
+            if(item.getCarNumber()==values )
+                return true;
+        }
         return false;
+
     }
 
     @Override
     public boolean ReturnCarModelById(Long values) {
+
+        for (CarModel item:carModelList) {
+            if(item.getCode()==values )
+                return true;
+        }
         return false;
     }
 
     @Override
     public boolean custumerExsits(Customer values) {
-           return  false;
+
+        for (Customer item:customerList) {
+            if(item.getId().equals(values.getId()) )
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public Boolean addCustomer(Customer values) {
 
         try {
-            List<Customer> result=getAllCustomers();
-            for (Customer item:result) {
-            if(item.getId().equals(values.getId()) )
-            {
-                return false;
-            }
-            }
 
+            if(custumerExsits(values)==true)
+                return false;
             String url = WEB_URL + "addCustomer.php" ;
 
             final ContentValues v = new ContentValues();
@@ -80,31 +104,100 @@ public class MySQL_DBManager implements DB_manager {
             v.put( "creditCard", values.getCreditCard() );
 
             PHPtools.POST( url, v );
+
         } catch (Exception e) {
             //Log.w( Constants.Log.APP_LOG, e.getMessage() );
         }
+        customerList.add(values);
         return true;
     }
 
     @Override
     public long addCarModel(CarModel values)
     {
+        try {
 
-return values.getCode();
+            if(ReturnCarModelById(values.getCode())==true)
+                return -1;
+            String url = WEB_URL + "addModel.php" ;
+
+            final ContentValues v = new ContentValues();
+            v.put( "_id", values.getCode() );
+            v.put( "company", values.getCompany() );
+            v.put( "model", values.getModel() );
+            v.put( "enginCapacity", values.getEngineCapacity() );
+            v.put( "gear", values.getGear().toString() );
+            v.put( "seats", values.getSeats() );
+
+            PHPtools.POST( url, v );
+
+        } catch (Exception e) {
+            //Log.w( Constants.Log.APP_LOG, e.getMessage() );
+        }
+        carModelList.add(values);
+        return values.getCode();
+
     }
 
     @Override
     public long addCar(Car values) {
-        return 0;
+
+        try {
+
+            if(ReturnCarModelById(values.getCarNumber())==true)
+                return -1;
+            String url = WEB_URL + "addCar.php" ;
+
+            final ContentValues v = new ContentValues();
+            v.put( "_id", values.getCarNumber() );
+            v.put( "branch", values.getBranchNumber() );
+            v.put( "model", values.getModel() );
+            v.put( "km", values.getKilometers() );
+            PHPtools.POST( url, v );
+
+        } catch (Exception e) {
+            //Log.w( Constants.Log.APP_LOG, e.getMessage() );
+        }
+        carList.add(values);
+        return values.getCarNumber();
+
     }
 
     @Override
     public List<CarModel> getAllModels() {
+
+        if(carModelList != null)
+            return carModelList;
+        List<CarModel> result = new ArrayList<CarModel>();
+        try
+        {
+            String str = PHPtools.GET(WEB_URL + "getModel.php");
+            JSONArray array = new JSONObject(str).getJSONArray("model");
+            for (int i = 0; i < array.length(); i++)
+            {
+                JSONObject jsonObject = array.getJSONObject(i);
+                CarModel model = new CarModel();
+                model.setCode(jsonObject.getInt("_id"));
+                model.setCompany(jsonObject.getString("company"));
+                model.setModel(jsonObject.getString("model"));
+                model.setEngineCapacity(jsonObject.getInt("enginCapacity"));
+                model.setGear(Gearbox.valueOf((jsonObject.getString("gear"))));
+                model.setSeats(jsonObject.getInt("seats"));
+                result.add(model);
+
+            }
+            return result;
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public List<Customer> getAllCustomers() {
+        if(customerList != null)
+            return customerList;
         List<Customer> result = new ArrayList<Customer>();
         try
         {
@@ -133,7 +226,8 @@ return values.getCode();
 
     @Override
     public List<Branch> getAllBrunches() {
-
+        if(branchList != null)
+            return branchList;
             List<Branch> result = new ArrayList<Branch>();
             try
             {
@@ -152,13 +246,37 @@ return values.getCode();
             } catch (Exception e)
             {
                 e.printStackTrace();
-            }     return null;
+            }
+            return null;
         }
 
 
 
     @Override
     public List<Car> getAllCars() {
+
+        if(carList != null)
+            return carList;
+        List<Car> result = new ArrayList<Car>();
+        try
+        {
+            String str = PHPtools.GET(WEB_URL + "getCar.php");
+            JSONArray array = new JSONObject(str).getJSONArray("car");
+            for (int i = 0; i < array.length(); i++)
+            {
+                JSONObject jsonObject = array.getJSONObject(i);
+                Car car = new Car();
+                car.setCarNumber(jsonObject.getInt("_id"));
+                car.setBranchNumber(jsonObject.getInt("branch"));
+                car.setModel(jsonObject.getInt("model"));
+                car.setKilometers(jsonObject.getInt("km"));
+                result.add(car);
+            }
+            return result;
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
         return null;
     }
 }
